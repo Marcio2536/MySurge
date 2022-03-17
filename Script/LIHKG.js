31294b1e5a764e89f7b73d4dc69e5efe8ec7ad7a
Edmond <61924136+Marcio2536@users.noreply.github.com>
2022-03-17 16:20:00 +0800

    Add files via upload

--- Script/LIHKG.js
+++ Script/LIHKG.js
@@ -1,3 +1,4 @@
+// LIHKG = type=http-response,pattern=https://lihkg.com/api_v2/thread,requires-body=1,max-size=-1,script-path=https://raw.githubusercontent.com/Marcio2536/MySurge/main/Script/LIHKG.js,script-update-interval=0
 let body = $response.body;
 body = body.replace(/"display_vote":false/g,'"display_vote":true');
 $done({body});
