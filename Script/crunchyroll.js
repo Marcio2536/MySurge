cbdfa741dae4cbe9b8cb0720bd6121796ed588e5
Edmond <61924136+Marcio2536@users.noreply.github.com>
2022-06-12 20:37:59 +0800

    Update crunchyroll.js

--- Script/crunchyroll.js
+++ Script/crunchyroll.js
@@ -1,3 +1,5 @@
+/* Blcok Crunchyroll Ads on iOS & iPadOS
+Crunchyroll Adblock = type=http-response,pattern=https://beta-api.crunchyroll.com/cms,requires-body=1,max-size=-1,control-api=1,script-path=https://raw.githubusercontent.com/Marcio2536/MySurge/main/Script/crunchyroll.js,script-update-interval=0 */
 let body = $response.body;
 body = body.replace(/"offset_ms":\d+/g, '"offset_ms":99999999999999');
 console.log(body)
