7f5f2e6f10cc7ff5987c7f71b2a7175b80e984df
Edmond <61924136+Marcio2536@users.noreply.github.com>
2022-06-12 20:35:45 +0800

    Update paramountplus.js

--- Script/paramountplus.js
+++ Script/paramountplus.js
@@ -1,4 +1,4 @@
-/*
+/* Enable Download on Canadian Paramount+ Account
 Paramount+ = type=http-response,pattern=https://www.intl.paramountplus.com/apps-api/v2.0/ipad/videos/section/\d+\.json,requires-body=1,max-size=0,script-path=https://raw.githubusercontent.com/Marcio2536/MySurge/main/Script/paramountplus.js,script-update-interval=0
  */
 let body = $response.body;
