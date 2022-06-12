b637275d866dea67f2a859ba333ea3d2f5a38f70
Edmond <61924136+Marcio2536@users.noreply.github.com>
2022-06-12 20:31:57 +0800

    Update discordjoin.js

--- Script/discordjoin.js
+++ Script/discordjoin.js
@@ -1,3 +1,5 @@
+/* Bypass Discord Restriction on Joining NSFW Groups
+Discord Join = type=http-response,pattern=https://discord.com/api/v9/invites,requires-body=1,max-size=-1,control-api=1,script-path=https://raw.githubusercontent.com/Marcio2536/MySurge/main/Script/discordjoin.js,script-update-interval=0 */
 let body = $response.body;
 body = body.replace(/"nsfw": true/,'"nsfw": false');
 body = body.replace(/"nsfw_level": \d/,'"nsfw_level": 0');
