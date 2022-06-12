6883f1324ede474c251ceea5809ef941563592a3
Edmond <61924136+Marcio2536@users.noreply.github.com>
2022-06-12 20:22:49 +0800

    Update Photoshop.js

--- Script/Photoshop.js
+++ Script/Photoshop.js
@@ -1,3 +1,6 @@
+/* Crack Photoshop App on iPad
+Photoshop = type=http-response,pattern=https:\/\/lcs-mobile-cops\.adobe\.io\/mobile_profile\/nul\/v2,requires-body=1,max-size=-1,control-api=1,script-path=https://raw.githubusercontent.com/Marcio2536/rules/master/Photoshop.js,script-update-interval=0  
+*/
 let body = JSON.parse($response.body)
 let pro = body["mobileProfile"];
 pro["profileStatus"] = "PROFILE_AVAILABLE";
